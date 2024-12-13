39f7204d62fe282c345e93a35618edd12cfd37fb
flozonn <74357383+flozonn@users.noreply.github.com>
2024-12-13 10:06:52 +0100

    pass parameters

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
-print("Hello!")
+import argparse
+import json
+import os
 
 
+hyperparams_path = "/opt/ml/input/config/hyperparameters.json"
+if os.path.exists(hyperparams_path):
+    with open(hyperparams_path, "r") as f:
+        hyperparameters = json.load(f)
+
+name = hyperparameters.get("name", None)
+print("Hello! "+ name)
